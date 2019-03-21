3955f64cc97acc2471ce182a6a7a4148ad8892b0
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-21 10:03:52 -0400

    Done?

--- MyDeque.java
+++ MyDeque.java
@@ -1,15 +1,73 @@
 public class MyDeque<E>{
-  private E[] data;
-  private int size, start, end;
+    private E[] data;
+    private int size, start, end;
 
-  public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+    @SuppressWarnings("unchecked")
+    public MyDeque(){
+        data = (E[])new Object[10];
+    }
+    @SuppressWarnings("unchecked")
+    public MyDeque(int initialCapacity){
+        data = (E[])new Object[initialCapacity];
+    }
+    public int size(){
+        return data.length;
+    }
+    public String toString(){
+        String display = "[";
+        int i = 0;
+        for (; i < data.length - 1; i ++){
+            display = display + data [i] + ", ";
+        }
+        if (i == data.length -1 ){
+            display = display + data[i] + "]";
+        }
+    }
+    public void addFirst(E element){
+            // if more space is needed
+        if (end == data.length -1){
+            E [] copy = new E [data.length * 2];
+            copy [0] = E;
+            for (int x = 1; x < data.length + 1; x ++){
+                copy [x] = data [x];
+            }
+            data = copy;
+        }
+        else {
+            data [start - 1] = E;
+            start --;
+        }
+    }
+
+    public void addLast(E element){
+        // if more space is needed
+        if (end == data.length -1){
+            E [] copy = new E [data.length * 2];
+            for (int x = 0; x < data.length; x ++){
+                copy [x] = data [x];
+            }
+            data = copy;
+        }
+        else {
+            data [end] = E;
+            end ++;
+        }
+    }
+
+    public E removeFirst(){
+        data [0] = 0;
+        start  ++;
+        return data;
+    }
+    public E removeLast(){
+        data[data.length-1] = 0;
+        end --;
+        return data;
+    }
+    public E getFirst(){
+        return data[0];
+    }
+    public E getLast(){
+        return data[0];
+    }
 }
