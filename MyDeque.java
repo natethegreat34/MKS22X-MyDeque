0a8e119523c096e903a0562dbd17bd1c408d0777
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-24 17:40:12 -0400

    Should be done, need a better driver though

--- MyDeque.java
+++ MyDeque.java
@@ -5,27 +5,42 @@ import java.util.*;
 public class MyDeque<E>{
     private E[] data;
     private int start, end;
-    private int size = end - start;
+    private int size = 0;
+    //constructors
     @SuppressWarnings("unchecked")
     public MyDeque(){
         data = (E[])new Object[10];
+        size = 0;
+        start = 0;
+        end = 0;
     }
+
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
         data = (E[])new Object[initialCapacity];
+        size = initialCapacity;
+        start = 0;
+        end = 0;
     }
+
     public int size(){
-        return size;
+        System.out.println(end + "  g g  " + start);
+        return end - start;
     }
+
     public String toString(){
         String display = "{";
+        if (size == 0){
+            display = display + "}";
+        }
+        else{
         int i = start;
-        for (; i < end; i ++){
+        for (; i < end - 1; i ++){
             if (data [i] != null){
                 display = display + data [i] + " ";
             }
         }
-        if (i == end){
+        if (i == end - 1){
             if (data [i] != null){
                 display = display + data[i] + "}";
             }
@@ -33,35 +48,43 @@ public class MyDeque<E>{
                 display = display +"}";
             }
         }
+    }
         return display;
     }
+
+
     public void addFirst(E element){
-        System.out.println("help " + data.length );
+        System.out.println("help " + size );
         if (element == null){
             throw new NullPointerException ("");
         }
+        // if the array has size 0
         if (size == 0){
             System.out.println("spy");
             data = (E[])new Object[10];
             data [0] = element;
-                size ++;
+            size = 10;
+            start = 0;
+            end = 1;
         }
         else{
+            // might need to replace null values
             // if more space is needed
-        if (end == data.length -1){
-            E[] copy = (E[])new Object[data.length * 2];
-            size = data.length * 2;
-            copy [0] = element;
-            for (int x = 1; x < data.length + 1; x ++){
-                copy [x] = data [x - 1];
+            // if the array is full
+            if (end == size && start == 0){
+                E[] copy = (E[])new Object[size * 2];
+                copy [0] = element;
+                for (int x = 1; x < size; x ++){
+                    copy [x] = data [x - 1];
+                }
+                size = size * 2;
+                data = copy;
+                end ++;
             }
-            data = copy;
-        }
-        else {
-            data [start - 1] = element;
-            start --;
-            size ++;
-        }
+            else {
+                data [start - 1] = element;
+                start --;
+            }
         }
     }
 
@@ -73,58 +96,64 @@ public class MyDeque<E>{
             System.out.println("spy");
             data = (E[])new Object[10];
             data [0] = element;
-            System.out.println(this.toString());
-            size ++;
+            size = 10;
+            start = 0;
+            end = 1;
         }
         else{
-        // if more space is needed
-        if (end == data.length -1){
-            E[] copy = (E[])new Object[data.length * 2];
-            size = data.length * 2;
-            for (int x = 0; x < data.length; x ++){
-                copy [x] = data [x];
+            // if more space is needed
+            if (end == size){
+                E[] copy = (E[])new Object[size * 2];
+                for (int x = 0; x < size; x ++){
+                    copy [x] = data [x];
+                }
+                copy[size] = element;
+                size = size * 2;
+                data = copy;
+                end ++;
             }
-            copy[data.length] = element;
-            data = copy;
-        }
-        else {
-            data [end] = element;
-            size ++;
-            end ++;
+            else {
+                data [end] = element;
+                end ++;
+            }
         }
     }
-}
 
     public E removeFirst(){
-        if (size == 0){
+        if (end - start == 0){
             throw new NoSuchElementException ("");
         }
-        E ho = data[size - 1];
-        data [0] = null;
-        start  ++;
-        size --;
+        E ho = data[start];
+        data [start] = null;
+        start ++;
         return ho;
     }
+
     public E removeLast(){
-        if (size == 0){
+        if (end - start == 0){
             throw new NoSuchElementException ("");
         }
-        E hey = data[data.length - 1];
-        data[data.length-1] = null;
+        E hey = data[end];
+        data[end] = null;
         end --;
-        size --;
         return hey;
     }
+
     public E getFirst(){
-        if (size == 0){
+        if (end - start == 0){
             throw new NoSuchElementException ("");
         }
-        return data[0];
+        else{
+        return data[start];
+        }
     }
+
     public E getLast(){
-        if (size == 0){
+        if (end - start == 0){
             throw new NoSuchElementException ("");
         }
-        return data[0];
+        else{
+        return data[end];
+        }
     }
 }
