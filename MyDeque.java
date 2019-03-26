6397f80c37928cc5a0494522192346bb3590f10d
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-25 20:40:23 -0400

    trying to fix addfirst

--- MyDeque.java
+++ MyDeque.java
@@ -17,23 +17,31 @@ public class MyDeque<E>{
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
         data = (E[])new Object[initialCapacity];
-        size = initialCapacity;
+        size = 0;
         start = 0;
         end = 0;
     }
 
     public int size(){
+
         // System.out.println(end + "  g g  " + start);
-        return end - start;
+        return size;
     }
 
     public String toString(){
-        String display = "{";
-        if (size == 0){
-            display = display + "}";
-        }
-        else{
         int i = start;
+        String display = "{";
+        if (data.length == 0){
+            display = display + "}";
+            return display;
+        }
+        if (start > end - 1){
+            for (; i < data.length; i ++){
+                if (data [i] != null){
+                    display = display + data [i] + " ";
+                }
+            }
+        }
         for (; i < end - 1; i ++){
             if (data [i] != null){
                 display = display + data [i] + " ";
@@ -47,25 +55,24 @@ public class MyDeque<E>{
                 display = display +"}";
             }
         }
-    }
         return display;
     }
 
 
     public void addFirst(E element){
         // the error is when the arraylist is [0, null, null, null]
-        // ^^end != size, because nulls make the size bigger
-        
-        // System.out.println("help " + size );
+        // ^^end != data.length, because nulls make the data.length bigger
+
+        // System.out.println("help " + data.length );
         if (element == null){
             throw new NullPointerException ("");
         }
-        // if the array has size 0
-        if (size == 0){
+        // if the array has data.length 0
+        if (data.length == 0){
             // System.out.println("spy");
             data = (E[])new Object[10];
             data [0] = element;
-            size = 10;
+            size = 1;
             start = 0;
             end = 1;
         }
@@ -79,11 +86,15 @@ public class MyDeque<E>{
                 for (int x = 1; x < size; x ++){
                     copy [x] = data [x - 1];
                 }
-                size = size * 2;
+                size ++;
                 data = copy;
                 end ++;
             }
             else {
+                if(start == 0 && end != data.length){
+                    start = data.length -1;
+                    data [data.length - 1] = element;
+                }
                 data [start - 1] = element;
                 start --;
             }
@@ -94,23 +105,23 @@ public class MyDeque<E>{
         if (element == null){
             throw new NullPointerException ("");
         }
-        if (size == 0){
+        if (data.length == 0){
             // System.out.println("spy");
             data = (E[])new Object[10];
             data [0] = element;
-            size = 10;
+            size = 1;
             start = 0;
             end = 1;
         }
         else{
             // if more space is needed
-            if (end == size){
-                E[] copy = (E[])new Object[size * 2];
-                for (int x = 0; x < size; x ++){
+            if (end == data.length){
+                E[] copy = (E[])new Object[data.length * 2];
+                for (int x = 0; x < data.length; x ++){
                     copy [x] = data [x];
                 }
-                copy[size] = element;
-                size = size * 2;
+                copy[data.length] = element;
+                size ++;
                 data = copy;
                 end ++;
             }
