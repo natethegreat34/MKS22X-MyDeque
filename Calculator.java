8c37cdfd9c0202103f2a67ed5994fd9e409878ee
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-28 07:30:26 -0400

    Accidentally uploaded driver, but this is the final version

--- Calculator.java
+++ Calculator.java
@@ -4,30 +4,52 @@ public class Calculator{
      */
     public static double eval(String s){
         String [] str = s.split(" ", 0);
-        MyDeque<Integer> holder = new MyDeque<>();
+        MyDeque<Double> holder = new MyDeque<>();
         for (int x = 0; x < str.length; x ++){
             if (str[x].equals("+")){
-                int pl = holder.removeLast() + holder.removeLast();
+                double b = holder.removeLast();
+                double a = holder.removeLast();
+                double pl = a + b;
                 holder.addLast(pl);
             }
             else if (str[x].equals("-")){
-                int mi = holder.removeLast() - holder.removeLast();
+                // needs to be in right order
+                // 100 6 - = 94 b/c 100 - 6 = 96
+                // but if it is 6 - 100 that = -96
+                // so have to go to the one before, the one before
+                double b = holder.removeLast();
+                double a = holder.removeLast();
+                double mi = a - b;
                 holder.addLast(mi);
             }
             else if (str[x].equals("*")){
-                int mu = holder.removeLast() * holder.removeLast();
+                double b = holder.removeLast();
+                double a = holder.removeLast();
+                double mu = a * b;
                 holder.addLast(mu);
             }
             else if (str[x].equals("/")){
-                int di = holder.removeLast() / holder.removeLast();
+                // needs to be in right order
+                // 100 25 / = 4 b/c 100 / 25 = 4
+                // but if it is 25 / 100 that = .25
+                // so have to go to the one before, the one before
+                double b = holder.removeLast();
+                double a = holder.removeLast();
+                double di = a / b;
                 holder.addLast(di);
             }
             else if (str[x].equals("%")){
-                int mo = holder.removeLast() % holder.removeLast();
+                // needs to be in right order
+                // 100 10 % = 10 b/c 100 10 % = 10
+                // but if it is 10 % 100 that = 0
+                // so have to go to the one before, the one before
+                double b = holder.removeLast();
+                double a = holder.removeLast();
+                double mo = a % b;
                 holder.addLast(mo);
             }
             else{
-            holder.addLast(Integer.parseInt(str[x]));
+            holder.addLast(Double.parseDouble(str[x]));
             }
         }
         return holder.getFirst();
